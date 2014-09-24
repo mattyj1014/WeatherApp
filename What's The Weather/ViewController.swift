be36b63443a1ce9f2b00cc9695be0f2d029c1931
Matthew Jenks <matthewjenks@Matthews-MacBook-Pro.local>
2014-09-24 16:10:11 -0700

    first v

--- What's The Weather/ViewController.swift
+++ What's The Weather/ViewController.swift
@@ -7,19 +7,64 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    
+    
+    @IBOutlet weak var city: UITextField!
+    
+    @IBOutlet weak var message: UILabel!
+    
+    @IBAction func buttonPressed(sender: AnyObject)
+    {
+        
+        self.view.endEditing(true)
+        
+        var urlString = "http://www.weather-forecast.com/locations/" + city.text.stringByReplacingOccurrencesOfString(" ", withString: "") + "/forecasts/latest"
+        
+        var url = NSURL(string: urlString)
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) {(data, response, error) in
+            
+            var urlContent = NSString(data: data, encoding: NSUTF8StringEncoding)
+            
+            
+            
+            if (urlContent as NSString).containsString("<span class=\"phrase\">") {
+            
+            
+            
+            var contentArray = urlContent.componentsSeparatedByString("<span class=\"phrase\">")
+        
+            
+            var newContentArray = contentArray[1].componentsSeparatedByString("</span>")
+            
+            self.message.text = newContentArray[0].stringByReplacingOccurrencesOfString("&deg;", withString: "º") as String
+            
+            } else {
+            
+            self.message.text = "Counln't find that city - please try again"
+            
+            }
+            
+        
+    }
+        task.resume()
+    
+    
+      override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
+      override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
 }
+
 
